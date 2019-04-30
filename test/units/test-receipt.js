6dce7b5a1d494f92ddd6534073320da9081d6643
Trent Sawicki <trentsawicki@gmail.com>
2019-04-30 15:39:05 -0400

    Began testing branch. Created basic unit test framework for all pages.

---
+++ test/units/test-receipt.js
@@ -0,0 +1,19 @@
+const {describe, it} = require('mocha')
+const {expect} = require('chai')
+//const student = require('../../src/pages/student.ts')
+
+describe('receipt', function () {
+
+  describe('constructor', function () {
+      it('Sample description', function () {
+
+      })
+  })
+/**
+  it('should show initial display correctly', () => {
+    //expect(calculator.initialState.display).to.equal('0')
+  })
+  it('should replace 0 in initialState', () => {
+    //expect(stream('4').display).to.equal('4')
+  }) */
+})
