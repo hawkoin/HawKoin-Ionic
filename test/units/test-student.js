fdd9659e728803e9e444ec2068d2472164539adb
Nick <nis220@lehigh.edu>
2019-10-29 11:53:09 -0400

    Made changes to student to reflect doc changes, need to update others but the Current/head needs to be rectified

--- test/units/test-student.js
+++ test/units/test-student.js
@@ -15,7 +15,31 @@ describe('student', function () {
   })
 
   describe('toggleBalance', function () {
-    
+    it('balance should display correctly', function () {
+
+    })
+    it('balance displayed should change after non-zero transaction', function () {
+
+    })
+  })
+  describe('Transactions', function () {
+    it('negative balance transactions are not allowed',function () {
+
+    })
+    it('balance is actually changed after transaction occurs', function () {
+
+    })
+  })
+  describe('Confirmation', function () {
+    it('confirmation is displayed to Spender',function () {
+
+    })
+    it('Confirm reflects a change to non-zero balance', function () {
+
+    })
+    it('Cancel reflects no change to balance', function() {
+
+    })
   })
 
   it('should show initial display correctly', () => {
